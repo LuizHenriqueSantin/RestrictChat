2b37b75d34aeddd95e563bb05795b23ee2bc6ea1
luizhenriquersantin@gmail.com <luizhenriquersantin@gmail.com>
2026-05-11 19:12:09 -0300

    auto run migrations

--- RestrictChat.Api/Program.cs
+++ RestrictChat.Api/Program.cs
@@ -68,6 +68,12 @@ public class Program
         app.UseAuthentication();
         app.UseAuthorization();
 
+        using (var scope = app.Services.CreateScope())
+        {
+            var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+            db.Database.Migrate();
+        }
+
         RegisterEndpoint.Map(app);
         LoginEndpoint.Map(app);
 
